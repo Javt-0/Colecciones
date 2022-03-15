1043d4b3720a5c40e3435b647e563dcc166abff5
Javt-0 <100586250+Javt-0@users.noreply.github.com>
2022-03-15 16:36:39 +0100

    Add files via upload

---
+++ ColeccionesDam/src/coleccionesdam/ColeccionesDam.java
@@ -0,0 +1,17 @@
+
+package coleccionesdam;
+
+/**
+ *
+ * @author jonat
+ */
+public class ColeccionesDam {
+
+   
+    public static void main(String[] args) {
+        System.out.println("hola");
+        
+        System.out.println("jejejeje");
+    }
+    
+}
